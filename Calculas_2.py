f84b0812c61773ca2eef6dcc3533cc3c5b319b80
Sahil Singh <125428501+Sahil-Singh2002@users.noreply.github.com>
2023-03-31 18:03:22 +0100

    Update and rename Calculas_coursework_2.py to Calculas_2.py

--- Calculas_2.py
+++ Calculas_2.py
@@ -1,38 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 #The code for the Function f(x,y)
-
 x = np.linspace(-1,1)
 y = np.linspace(-1,1)
 X,Y = np.meshgrid(x,y)
 Z = (X*Y)/np.exp(2*(X**2)+(Y**4))
 fig = plt.figure()
-
 #the Labeling code:
-
 ax = fig.add_subplot(111,aspect='equal') 
 ax.contour(X,Y,Z)
 ax.set_xlabel("The X axies")
 ax.set_ylabel("The y axies")
 ax.set_title("Level curve of f")
 plt.show()
-
 #FILE
-
 fig.savefig('outputimage.png')
-
-import numpy as np
-import matplotlib.pyplot as plt
-# Your code here
+#_________________________________________________________
 x = np.linspace(-1,1)
 y = np.linspace(-1,1)
 X,Y = np.meshgrid(x,y)
 Z = (X*Y)/np.exp(2*(X**2)+(Y**4))
 fig = plt.figure(figsize=(15,5))
-
 #code for ploting the graph and labels:
-
 ax = fig.add_subplot(111,projection='3d')
 ax.plot_surface(X, Y, Z)
 ax.set_title("3D surface plot")
@@ -40,32 +29,23 @@ ax.contour(X,Y,Z)
 ax.set_xlabel("x axies")
 ax.set_ylabel("y axies")
 ax.set_zlabel("z axies")
-
 #FILE
-
 fig.savefig('outputimage.png')
-
-import numpy as np
-import matplotlib.pyplot as plt
-def draw_plot(a,tmax,n):    
-    # Your code here
+#________________________________________________________
+def draw_plot(a,tmax,n):
     t = np.linspace(0,tmax,n)
     x= np.cos(a*t)    
     y= np.cos(t)*(4+np.sin(a*t))    
     z= np.sin(t)*(4+np.sin(a*t))
+    #plot
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-
     ax.plot(x, y, z,'black',lw='1.25')
     ax.set_xlabel("x axies")
     ax.set_ylabel("y axies")
     ax.set_zlabel("z axies")
     ax.set_title("Parametric plot of f")
     return fig
-  
 figoutput=draw_plot(2*np.pi,20*np.pi,1000)
-
 #FILE
-
 figoutput.savefig('outputimage.png')
-
